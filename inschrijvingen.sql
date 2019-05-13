ab59e422d0efacdbd269cb6435d94662c2b3df39
offie321 <43699906+offie321@users.noreply.github.com>
2019-05-13 21:29:48 +0200

    Opdracht 6

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -75,3 +75,37 @@ VALUES (93023,'lo9e-amo1'),
 UPDATE studenten
 SET actief = FALSE
 WHERE ov_nummer IN (86046, 84907);
+
+/*Opdracht 6*/
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES ('lo9e-amo1','bkr02', 2019, 'amo'),
+	     ('lo9e-amo2','vlt03', 2019, 'amo'),
+       ('lo9e-amo3','rs001', 2019, 'amo');
+
+
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES (93023,'Pieterszoon','','Pieter','1994-05-20','2231 ZX','Rijnsburg', 75.2, 1.43, '2018-01-01'),
+       (94269,'Janssen','','Jan','1996-06-18','2408 JV','Alphen a/d Rijn', 69.4, 1.98, '2018-01-01'),
+       (95832,'Samson','','Gert','1997-02-14','2265 XR','Leidschendam', 60.1, 1.87, '2018-01-01'),
+       (98416,'Groenen','','Barry','1995-08-15','2101 VW','Hardewijk', 70.4, 1.73, '2018-01-01');
+
+INSERT INTO klassen_studenten(ov_nummer, klas_code)
+VALUES (93023,'lo9e-amo1'),
+	     (94269,'lo9e-amo3'),
+	     (95832,'lo9e-amo2'),
+	     (98416,'lo9e-amo2');
+
+UPDATE studenten
+SET actief = FALSE
+WHERE ov_nummer IN (86046, 84907);
+
+SELECT studenten.ov_nummer, studenten.voornaam, studenten.tussenvoegsel, studenten.achternaam, klassen.klas_code, klassen.cohort, docenten.voorletters, docenten.tussenvoegsel, docenten.achternaam
+FROM studenten
+left JOIN klassen_studenten
+ON studenten.ov_nummer = klassen_studenten.ov_nummer
+left JOIN klassen
+ON klassen_studenten.klas_code = klassen.klas_code
+left JOIN docenten
+ON klassen.slb_code = docenten.docent_code
+ORDER BY klassen.klas_code DESC,
+		 docenten.achternaam DESC;
