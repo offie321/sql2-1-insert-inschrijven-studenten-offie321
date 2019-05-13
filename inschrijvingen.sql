54643c882ba44e3f654ed5c81eb7a20292b478fc
offie321 <43699906+offie321@users.noreply.github.com>
2019-05-13 20:06:42 +0200

    Opdracht 1

---
+++ inschrijvingen.sql
@@ -0,0 +1,5 @@
+INSERT INTO klassen
+    (klas_code, slb_code, cohort, opleiding_code) VALUES
+    ('lo9e-amo1','bkr02', 2019, 'amo'),
+    ('lo9e-amo2','vlt03', 2019, 'amo'),
+    ('lo9e-amo3','rs001', 2019, 'amo');
