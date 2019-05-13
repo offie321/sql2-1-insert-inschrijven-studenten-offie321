fd07a7d3f516ffd260d38e6750c12a336bb7c62d
offie321 <43699906+offie321@users.noreply.github.com>
2019-05-13 21:13:12 +0200

    Opdracht 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,11 @@
 INSERT INTO klassen
-    (klas_code, slb_code, cohort, opleiding_code) VALUES
-    ('lo9e-amo1','bkr02', 2019, 'amo'),
-    ('lo9e-amo2','vlt03', 2019, 'amo'),
-    ('lo9e-amo3','rs001', 2019, 'amo');
+    (klas_code, slb_code, cohort, opleiding_code)
+    VALUES  ('lo9e-amo1','bkr02', 2019, 'amo'),
+            ('lo9e-amo2','vlt03', 2019, 'amo'),
+            ('lo9e-amo3','rs001', 2019, 'amo');
+
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES  (93023,'Pieterszoon','','Pieter','1994-05-20','2231 ZX','Rijnsburg', 75.2, 1.43, '2018-01-01'),
+    	  (94269,'Janssen','','Jan','1996-06-18','2408 JV','Alphen a/d Rijn', 69.4, 1.98, '2018-01-01'),
+        (95832,'Samson','','Gert','1997-02-14','2265 XR','Leidschendam', 60.1, 1.87, '2018-01-01'),
+        (98416,'Groenen','','Barry','1995-08-15','2101 VW','Hardewijk', 70.4, 1.73, '2018-01-01');
